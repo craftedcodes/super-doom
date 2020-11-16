88e00a4763b284c805af23579293ce3952596c00
Marius Moeller <viktor.rellom@protonmail.com>
2020-11-16 18:16:31 +0100

    working on superpowers

--- app/models/superpower.rb
+++ app/models/superpower.rb
@@ -1,5 +1,7 @@
 class Superpower < ApplicationRecord
   has_many :rents
+  belongs_to :user
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { only_integer: true }
+  validates :description, presence: true
 end
