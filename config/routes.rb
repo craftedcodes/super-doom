32f7628b009e4fbe0bef1337aaef91adec8ddd5d
Marius Moeller <viktor.rellom@protonmail.com>
2020-11-17 11:49:01 +0100

    idk what i did doesnt matter for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  get '/superpowers', to: 'superpowers#index'
-  get '/superpowers/new', to: 'superpowers#new'
-  post '/superpowers', to: 'superpowers#create'
-  get '/users/:id', to: 'users#show', as: :user
+ # get '/superpowers', to: 'superpowers#index'
+ # get '/superpowers/new', to: 'superpowers#new'
+ # post '/superpowers', to: 'superpowers#create'
+ # get '/users/:id', to: 'users#show', as: :user
+
+resources :superpowers do
+  resources :rents, only: [:create, :show, :index]
 end
