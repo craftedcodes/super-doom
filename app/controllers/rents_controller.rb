20f8714dd39608532459d861f303e934a4d88962
Ilia Colombini <ilia.colombini@gmail.com>
2020-11-17 16:37:09 +0100

    Rents controller and view implemented, committing to retrieve the latest code

--- app/controllers/rents_controller.rb
+++ app/controllers/rents_controller.rb
@@ -1,2 +1,28 @@
 class RentsController < ApplicationController
+  def index 
+    @rents = Rent.all
+  end
+  
+  def create
+    @superpower = Superpower.find(params[:superpower_id])
+    @rent = Rent.new(rent_params)
+    @rent.superpower = @superpower 
+    if @rent.save 
+      redirect_to @superpower 
+    else 
+      render "superpowers/show"
+    end
+  end
+  
+  #to cancel a reservation
+  def destroy
+    @rent = Rent.find(params[:id])
+    @rent.destroy
+    redirect_to superpowers_path
+  end
+
+  private
+  def rent_params
+    params.require(:rent).permit(:date_from, :date_to)
+  end
 end
