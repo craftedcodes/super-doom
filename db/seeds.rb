82eb9258f96b4fca5a702429d188edbaeaa9752f
Marius Moeller <viktor.rellom@protonmail.com>
2020-11-18 13:13:19 +0100

    map & working on seeds(incomplete)

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "creating superpowers"
+
+20.times do
+  name = Faker::Games::LeagueOfLegends.summoner_spell
+  description = Faker::Games::LeagueOfLegends.quote
+  price = rand(1..100)
+  address = Faker::Address.country
+  Superpower.create(name: name, description: description, user_id: 1, price: price, location: address)
+end
+
+puts "created superpowers"
